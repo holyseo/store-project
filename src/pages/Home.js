6606940df9c3eb487c0b287f016ea30136a3d9fa
holyseo <128103221+holyseo@users.noreply.github.com>
2024-05-20 12:56:57 +0100

    add a router to Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Hero from "../components/hero";
+import CategoryCard from "../components/categoryCard";
+import MensClothing from "../assets/img/men-clothe.png";
+import WomensClothing from "../assets/img/women-clothe.png";
+import "./styles.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <hr></hr>
+      <h2>Categories</h2>
+      <div className="cat-cont">
+        <CategoryCard imgSrc={MensClothing} title={"Men's"} />
+        <CategoryCard imgSrc={WomensClothing} title={"Women's"} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Home from "./pages/Home";
+
+export const appRoutes = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
