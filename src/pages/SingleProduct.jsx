47fd6f29b18507d7ab4962db21ee08b34074c037
holyseo <128103221+holyseo@users.noreply.github.com>
2024-05-23 10:09:47 +0100

    add cart button on single page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleProduct } from "../utils";
 
-const SingleProduct = () => {
+const SingleProduct = ({ setCartItems }) => {
   const { id } = useParams();
   const [product, setProduct] = useState("");
+  const title = product.title;
+  const price = product.price;
+  const description = product.description;
+  const productName = product.productName;
+  const category = product.category;
+  const img = product.image;
 
   useEffect(() => {
     getSingleProduct(id);
@@ -15,7 +21,46 @@ const SingleProduct = () => {
     setProduct(response);
   };
 
-  return <div>{product.title}</div>;
+  return (
+    <div className="single-product-cont">
+      <div>
+        <img
+          className="product-image"
+          src={product.image}
+          alt="product-detail"
+        />
+      </div>
+      <div className="product-detail-cont">
+        <div className="product-title">{title}</div>
+        <div className="product-description">{description}</div>
+        <div className="product-category">{category}</div>
+        <div className="product-price">Price: ${price}</div>
+        <button
+          className="addCart-link"
+          onClick={() =>
+            setCartItems((initState) => {
+              return {
+                totalAmount: initState.totalAmount + price,
+                numberOfItems: initState.numberOfItems + 1,
+                cartItems: [
+                  ...initState.cartItems,
+                  {
+                    id: id,
+                    productName: productName,
+                    description: description,
+                    price: price,
+                    img: img,
+                  },
+                ],
+              };
+            })
+          }
+        >
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
